fccceabb823abf2aac4960a2112db39eb614857f
William Daigle <daiglewilliam13@gmail.com>
2024-05-29 17:01:12 -0700

    create utils file, move functions

---
+++ src/app/dashboard/utils.ts
@@ -0,0 +1,54 @@
+export async function getToken(url:any, apiKey:any) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        key: apiKey,
+      }),
+    };
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        console.log(await response.json())
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error logging in:', error);
+      throw error; // Re-throw for handling in the calling code
+    }
+  }
+  
+  export async function findAll(accessToken:any) {
+    const url = process.env.NEXT_PUBLIC_FINDALL_URL;
+  
+    const body = JSON.stringify({
+      dataSource: "social-media-clone",
+      database: "bug-tracker",
+      collection: "bugs",
+      filter: {},
+    });
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body,
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        console.log(response.json())
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching document:', error);
+      throw error; // Re-throw for handling in the calling code
+    }
+  }
