a4e8f56e30eeb9571535fee7d0a9ba4c67c67158
William Daigle <daiglewilliam13@gmail.com>
2024-06-19 18:13:27 -0700

    add highlight to current filter button

--- src/components/BugList.tsx
+++ src/components/BugList.tsx
@@ -7,7 +7,7 @@ import { getToken, findAll, blankBug } from "@/app/dashboard/utils";
 import { useState, useEffect } from 'react';
 
 export function BugList({currentUser, list}:any) {
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState('assigned');
   const [addBug, setAddBug] = useState(false);
 
   let editOptions = {
@@ -32,19 +32,27 @@ export function BugList({currentUser, list}:any) {
   const changeEditStatus = () => {
     setAddBug(addBug => !addBug);
   }
+
+  let adminStatus = currentUser.isAdmin;
   let formattedBugList = createBugList(list);
 useEffect(()=>{
-  
-
-},[filter, list])
+  let buttonList = Array.from(document.getElementsByClassName('filter-button'));
+  buttonList.forEach((button)=>{
+    if(filter==button.id) {
+      button.classList.add('highlight')
+    } else {
+      button.classList.remove('highlight');
+    }
+  })
+},[filter, list, currentUser])
 if (list){
   return (
     <div>
         <div>
-          { currentUser.isAdmin ? <button onClick={()=> setFilter('all')}>All Bugs</button> : <div></div> }
-          <button onClick={()=> setFilter('assigned')}>Assigned Bugs</button>
-          <button onClick={()=> setFilter('resolved')}>Resolved Bugs</button>
-          {currentUser.isAdmin==false? <button onClick={changeEditStatus}>Add Bug</button> : <div></div> }
+          { adminStatus ? <button id='all' className={'filter-button'} onClick={()=> setFilter('all')}>All Bugs</button> : <div></div> }
+          <button id='assigned' className={'filter-button'} onClick={()=> setFilter('assigned')}>Assigned Bugs</button>
+          <button id='resolved'className={'filter-button'} onClick={()=> setFilter('resolved')}>Resolved Bugs</button>
+          { adminStatus ? <button id='add' className={'filter-button'} onClick={changeEditStatus}>Add Bug</button> : <div></div> }
         </div>
         <div>
           <BugInput bugToEdit={blankBug} editOptions={editOptions} />
